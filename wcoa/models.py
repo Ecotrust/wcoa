fcb5704f8d04d3ca36e3e8e3bbc7fd751ac5f807
David Pollard <pollardld@gmail.com>
2019-07-19 17:26:25 -0700

    Add masonry page with loop for content bricks. count adds span class for masonry layout

--- wcoa/models.py
+++ wcoa/models.py
@@ -6,17 +6,34 @@ from wagtail.core import blocks
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.images.blocks import ImageChooserBlock
 
+class LinkStructValue(blocks.StructValue):
+    def url(self):
+        external_url = self.get('external_url')
+        page = self.get('page')
+        if external_url:
+            return external_url
+        elif page:
+            return page.url
+
+class BrickBlock(blocks.StructBlock):
+    title = blocks.CharBlock()
+    text = blocks.RichTextBlock()
+    page = blocks.PageChooserBlock(label="page", required=False)
+    external_url = blocks.URLBlock(label="external URL", required=False)
+    photo = ImageChooserBlock(required=False)
+
+    class Meta:
+        icon = 'page'
+        value_class = LinkStructValue
+
 class MasonryPage(Page):
-    author = models.CharField(max_length=255)
-    date = models.DateField("Post date")
     body = StreamField([
-        ('heading', blocks.CharBlock(classname="full title")),
-        ('paragraph', blocks.RichTextBlock()),
+        ('heading', blocks.CharBlock(classname="block-title")),
+        ('text', blocks.RichTextBlock()),
+        ('brick', BrickBlock()),
         ('image', ImageChooserBlock()),
     ])
 
     content_panels = Page.content_panels + [
-        FieldPanel('author'),
-        FieldPanel('date'),
         StreamFieldPanel('body'),
     ]
