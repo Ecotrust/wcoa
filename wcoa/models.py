8787bc28343981e77eff1d435a6cf1fe13c48eff
pollardld <pollardld@gmail.com>
2019-07-23 14:04:48 -0700

    Add connect model. Add connect template. Add connect fields

--- wcoa/models.py
+++ wcoa/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
-# Create your models here.
+from modelcluster.fields import ParentalKey
+
+from wagtail.core.models import Page, Orderable
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
+
+
+class ConnectPage(Page):
+
+    # Database fields
+
+    grid_cta_one = RichTextField()
+    grid_cta_two = RichTextField()
+    grid_cta_three = RichTextField()
+
+    body = RichTextField()
+    body_image = cover = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    date = models.DateField("Post date")
+    feed_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    # Search index configuration
+
+    search_fields = Page.search_fields + [
+        index.SearchField('body'),
+        index.FilterField('date'),
+    ]
+
+    # Editor panels configuration
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldRowPanel([
+            'grid_cta_three',
+            'grid_cta_three',
+            'grid_cta_three',
+        ]),
+        FieldPanel('body', classname="full"),
+        ImageChooserPanel('body_image'),
+    ]
+
+    promote_panels = [
+        MultiFieldPanel(Page.promote_panels, "Common page configuration"),
+        ImageChooserPanel('feed_image'),
+    ]
