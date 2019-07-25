6238c97539de4f6ee85ffcedbb7a650902cae925
David Pollard <pollardld@gmail.com>
2019-07-25 13:19:25 -0700

    Moved import/export urls to marco repo

--- wcoa/urls.py
+++ wcoa/urls.py
@@ -2,13 +2,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 from . import views
-from wagtailimportexport import urls as wagtailimportexport_urls
 
 app_name = 'wcoa'
 urlpatterns = [
     # path('', views.index, name='index'),
     path('/', views.index, name='index'),
-    # ...
-    url(r'', include(wagtailimportexport_urls)),
-    url(r'', include(wagtail_urls)),
 ]
