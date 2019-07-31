db37b5cd61314ebc1e430eb4b5118aee11cb7dbc
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-07-31 11:44:12 -0700

    overriding visualize to apply wcoa styles

--- wcoa/urls.py
+++ wcoa/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from .views import show_wcoa_planner
 
 from . import views
 
@@ -7,4 +8,6 @@ app_name = 'wcoa'
 urlpatterns = [
     # path('', views.index, name='index'),
     path('/', views.index, name='index'),
+    re_path(r'^planner/', show_wcoa_planner, name="planner_planner"),
+    re_path(r'^visualize/', show_wcoa_planner, name="planner"),
 ]
