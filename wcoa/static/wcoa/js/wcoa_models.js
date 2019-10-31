c9ff27c5e2d159836c0bee9e1ba85a0eea02bf44
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-10-30 17:12:47 -0700

    overriding logic for bitly shortening to use API v4

--- wcoa/static/wcoa/js/wcoa_models.js
+++ wcoa/static/wcoa/js/wcoa_models.js
@@ -12,3 +12,41 @@ app.viewModel.toggleLayers = function() {
     //self.correctTourPosition();
     //throws client-side error in pageguide.js for some reason...
 };
+
+app.viewModel.mapLinks.getURL = function() {
+    if (window.location.hostname == "localhost") {
+      return window.location.protocol + '//portal.westcoastoceans.org' + app.viewModel.currentURL();
+    } else {
+      return window.location.origin + app.viewModel.currentURL();
+    }
+};
+
+app.viewModel.mapLinks.useShortURL = function() {
+  var self = app.viewModel.mapLinks;
+  var bitly_login = "wcoasupport",
+      bitly_access_token = 'd46db74df0e396e4d52e82115ebdf3659f5115f5',
+      long_url = self.getURL();
+
+  var params = {
+    "long_url" : long_url
+  }
+
+  $.ajax({
+        url: "https://api-ssl.bitly.com/v4/shorten",
+        cache: false,
+        dataType: "json",
+        method: "POST",
+        contentType: "application/json",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", "Bearer " + bitly_access_token);
+        },
+        data: JSON.stringify(params)
+    }).done(function(response) {
+        $('.in #short-url')[0].value = response.link;
+
+    }).fail(function(data) {
+        console.log(data);
+    });
+
+
+};
