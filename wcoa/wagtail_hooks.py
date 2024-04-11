52672b9ff5167bf86ee0920236f17e09b69d45b1
David Pollard <dpollard@ecotrust.org>
2024-04-11 16:30:07 -0700

    Add wagtail hooks file and intial version of center align text

---
+++ wcoa/wagtail_hooks.py
@@ -0,0 +1,88 @@
+"""Richtext hooks."""
+import wagtail.admin.rich_text.editors.draftail.features as draftail_features
+from wagtail.admin.rich_text.converters.html_to_contentstate import (
+    InlineStyleElementHandler
+)
+from wagtail import hooks
+
+
+@hooks.register("register_rich_text_features")
+def register_heading_styling(features):
+    """Add a heading style to the richtext editor and page."""
+
+    # Step 1
+    feature_name = "h1"
+    type_ = "HEADING"
+    tag = "h1"
+
+    # Step 2
+    control = {
+        "type": type_,
+        "label": "h1",
+        "description": "h1"
+    }
+
+    # Step 3
+    features.register_editor_plugin(
+        "draftail", feature_name, draftail_features.InlineStyleFeature(control)
+    )
+
+    # Step 4
+    db_conversion = {
+        "from_database_format": {tag: InlineStyleElementHandler(type_)},
+        "to_database_format": {"style_map": {type_: {"element": tag}}}
+    }
+
+    # Step 5
+    features.register_converter_rule("contentstate", feature_name, db_conversion)
+
+    # Step 6. This is optional
+    # This will register this feature with all richtext editors by default
+    features.default_features.append(feature_name)
+
+
+@hooks.register("register_rich_text_features")
+def register_centertext_feature(features):
+    """Creates centered text in our richtext editor and page."""
+
+    # Step 1
+    feature_name = "center"
+    type_ = "CENTERTEXT"
+    tag = "div"
+
+    # Step 2
+    control = {
+        "type": type_,
+        "label": "center",
+        "description": "Center Text",
+        "style": {
+            "display": "block",
+            "text-align": "center",
+        },
+    }
+
+    # Step 3
+    features.register_editor_plugin(
+        "draftail", feature_name, draftail_features.InlineStyleFeature(control)
+    )
+
+    # Step 4
+    db_conversion = {
+        "from_database_format": {tag: InlineStyleElementHandler(type_)},
+        "to_database_format": {
+            "style_map": {
+                type_: {
+                    "element": tag,
+                    "props": {
+                        "class": "d-block text-center"
+                    }
+                }
+            }
+        }
+    }
+
+    # Step 5
+    features.register_converter_rule("contentstate", feature_name, db_conversion)
+
+    # Step 6, This is optional.
+    features.default_features.append(feature_name)
