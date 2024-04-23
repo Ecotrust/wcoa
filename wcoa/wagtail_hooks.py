39b44fbc6e8bd5988021b7b2e9ac982858672bef
David Pollard <dpollard@ecotrust.org>
2024-04-22 18:38:24 -0700

    center align wagtail hook. Icon in dev

--- wcoa/wagtail_hooks.py
+++ wcoa/wagtail_hooks.py
@@ -5,55 +5,46 @@ from wagtail.admin.rich_text.converters.html_to_contentstate import (
 )
 from wagtail import hooks
 
-
 @hooks.register("register_rich_text_features")
 def register_heading_styling(features):
     """Add a heading style to the richtext editor and page."""
 
-    # Step 1
     feature_name = "h1"
     type_ = "HEADING"
     tag = "h1"
 
-    # Step 2
     control = {
         "type": type_,
         "label": "h1",
         "description": "h1"
     }
 
-    # Step 3
     features.register_editor_plugin(
         "draftail", feature_name, draftail_features.InlineStyleFeature(control)
     )
 
-    # Step 4
     db_conversion = {
         "from_database_format": {tag: InlineStyleElementHandler(type_)},
         "to_database_format": {"style_map": {type_: {"element": tag}}}
     }
 
-    # Step 5
     features.register_converter_rule("contentstate", feature_name, db_conversion)
 
-    # Step 6. This is optional
     # This will register this feature with all richtext editors by default
     features.default_features.append(feature_name)
 
 
 @hooks.register("register_rich_text_features")
 def register_centertext_feature(features):
-    """Creates centered text in our richtext editor and page."""
+    """Add centered text option to richtext editor"""
 
-    # Step 1
     feature_name = "center"
     type_ = "CENTERTEXT"
     tag = "div"
 
-    # Step 2
     control = {
         "type": type_,
-        "label": "center",
+        "icon": [["M 0,100 1024,100"], ["M 0,500 1024,500"], ["M 0,900 1024,900"]],
         "description": "Center Text",
         "style": {
             "display": "block",
@@ -61,12 +52,10 @@ def register_centertext_feature(features):
         },
     }
 
-    # Step 3
     features.register_editor_plugin(
         "draftail", feature_name, draftail_features.InlineStyleFeature(control)
     )
 
-    # Step 4
     db_conversion = {
         "from_database_format": {tag: InlineStyleElementHandler(type_)},
         "to_database_format": {
@@ -81,8 +70,7 @@ def register_centertext_feature(features):
         }
     }
 
-    # Step 5
     features.register_converter_rule("contentstate", feature_name, db_conversion)
 
-    # Step 6, This is optional.
+    # This will register this feature with all richtext editors by default
     features.default_features.append(feature_name)
