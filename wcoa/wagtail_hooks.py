c403f6b227425bddd133ef215ae08356c0648920
David Pollard <dpollard@ecotrust.org>
2024-04-24 17:58:35 -0700

    Center hook for rich text editor.

--- wcoa/wagtail_hooks.py
+++ wcoa/wagtail_hooks.py
@@ -1,38 +1,6 @@
-"""Richtext hooks."""
-import wagtail.admin.rich_text.editors.draftail.features as draftail_features
-from wagtail.admin.rich_text.converters.html_to_contentstate import (
-    InlineStyleElementHandler
-)
 from wagtail import hooks
-
-@hooks.register("register_rich_text_features")
-def register_heading_styling(features):
-    """Add a heading style to the richtext editor and page."""
-
-    feature_name = "h1"
-    type_ = "HEADING"
-    tag = "h1"
-
-    control = {
-        "type": type_,
-        "label": "h1",
-        "description": "h1"
-    }
-
-    features.register_editor_plugin(
-        "draftail", feature_name, draftail_features.InlineStyleFeature(control)
-    )
-
-    db_conversion = {
-        "from_database_format": {tag: InlineStyleElementHandler(type_)},
-        "to_database_format": {"style_map": {type_: {"element": tag}}}
-    }
-
-    features.register_converter_rule("contentstate", feature_name, db_conversion)
-
-    # This will register this feature with all richtext editors by default
-    features.default_features.append(feature_name)
-
+import wagtail.admin.rich_text.editors.draftail.features as draftail_features
+from wagtail.admin.rich_text.converters.html_to_contentstate import InlineStyleElementHandler
 
 @hooks.register("register_rich_text_features")
 def register_centertext_feature(features):
@@ -44,12 +12,9 @@ def register_centertext_feature(features):
 
     control = {
         "type": type_,
-        "icon": [["M 0,100 1024,100"], ["M 0,500 1024,500"], ["M 0,900 1024,900"]],
+        "icon": ["M4 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"],
         "description": "Center Text",
-        "style": {
-            "display": "block",
-            "text-align": "center",
-        },
+        "style": {"display": "block", "text-align": "center"},
     }
 
     features.register_editor_plugin(
