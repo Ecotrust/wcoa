c0ef0ccbe9baa94fc4c5f64a987835b7feca9b59
David Pollard <pollardld@gmail.com>
2025-02-04 16:54:27 -0800

    Move http status column

--- wcoa/admin.py
+++ wcoa/admin.py
@@ -15,7 +15,7 @@ class WCOAThemeAdmin(ThemeAdmin):
     )
 
 class WCOALayerAdmin(LayerAdmin):
-    list_display = ('name', 'http_status', 'layer_type', 'date_modified', 'Theme_', 'order', 'data_source', 'primary_site', 'preview_site', 'url')
+    list_display = ('name', 'layer_type', 'date_modified', 'Theme_', 'order', 'data_source', 'primary_site', 'preview_site', 'http_status', 'url')
     if settings.CATALOG_TECHNOLOGY not in ['Madrona', None]:
         # catalog_fields = ('catalog_name', 'catalog_id',)
         catalog_fields = 'catalog_name'
